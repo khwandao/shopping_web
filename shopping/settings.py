01b2d9be7effb3ab2482f2f913bafba9982757ca
khwandao <khwandao.aitsara@gmail.com>
2022-11-02 21:09:36 +0700

    add database url

--- shopping/settings.py
+++ shopping/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'shopping/static')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'shopping/static'), ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
