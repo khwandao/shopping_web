4a5fd190573116a3a08f538e476c3c444e0f88c0
khwandao <khwandao.aitsara@gmail.com>
2022-11-02 20:50:42 +0700

    add database url

--- shopping/settings.py
+++ shopping/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [BASE_DIR / "static", '/var/www/static/',]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # STATIC_ROOT = "/var/www/example.com/static/"
 
