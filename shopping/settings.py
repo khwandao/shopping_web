259bd71bb468eaef4f423fb0b1dc773380a19bb8
khwandao <khwandao.aitsara@gmail.com>
2022-11-03 05:34:16 +0700

    add database url

--- shopping/settings.py
+++ shopping/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8t@!^mp_tzjtnmny!&0@029#=j-0o0oc79q=vf+^b(vt)k&but'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['https://shielded-stream-26535.herokuapp.com/']
 ALLOWED_HOSTS = ['*']
